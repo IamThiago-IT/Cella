9ee457e30ce2dcedfca928289f920e1ca37357a0
IamThiago-TI <53235884+IamThiagoff@users.noreply.github.com>
2020-04-12 17:05:29 -0300

    Conexão

--- conexao.php
+++ conexao.php
@@ -1,12 +1,11 @@
 <?php
-
 	/*
 	Professor Cezar Jenzura
 	conexao.php
 	Arquivo para conexão com o banco de dados
 	*/
 	try{ //tente
-		$fusca = new PDO("mysql:host=localhost;u277754222_cella123","u277754222_cella123","cella123");
+		$fusca = new PDO("mysql:host=localhost;dbname=u277754222_cella","u277754222_cella123","cella123");
 		//echo "Conexão efetuada com sucesso";
 	} 
 	catch(PDOException $e){ //Bloco correspondente ao try	
@@ -14,5 +13,4 @@
 		// verificar método echo $e->getCode(); 
 		echo $e->getMessage(); //método amplamente utilizado		
 	}
-
 ?>	
